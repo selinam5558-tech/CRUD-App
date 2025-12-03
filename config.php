bbbe7b66113f572a437214b24a8990dd37e2a6d5
selinam5558-tech <selinam5558@tamcc.edu.gd>
2025-12-03 12:42:26 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,29 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
+/* Aiven MySQL connection */
 define('DB_SERVER', 'crud-app-selinamcintyre07-2322.j.aivencloud.com');
+define('DB_PORT', 2322); // Aiven MySQL port
 define('DB_USERNAME', 'avnadmin');
 define('DB_PASSWORD', 'AVNS_6byV08x4wxGeggB_4Uq');
 define('DB_NAME', 'crud_app');
- 
-/* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
-// Check connection
-if($link === false){
+
+/* Create mysqli object */
+$link = mysqli_init();
+
+/* Aiven requires SSL */
+mysqli_ssl_set($link, NULL, NULL, __DIR__ . "/ca.pem", NULL, NULL);
+
+/* Connect */
+if (!mysqli_real_connect(
+    $link,
+    DB_SERVER,
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_NAME,
+    DB_PORT,
+    NULL,
+    MYSQLI_CLIENT_SSL
+)) {
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+
 ?>
